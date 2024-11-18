efef73106bf61fe25e6bba634ee1b3ad21f9bf31
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-11-18 15:24:20 +0600

    💅basic placement added

--- react/src/views/Dashboard.tsx
+++ react/src/views/Dashboard.tsx
@@ -5,20 +5,20 @@ export default function Dashboard() {
   const [Right, setRight] = useState("");
   return (
     <main className="container grid grid-cols-12 grid-rows-12 gap-2">
-      <div className="left">
+      <div className="left col-start-1 col-span-2 row-start-1 row-span-10 flex flex-col gap-4 p-4">
         <div className="icons">icon1</div>
         <div className="icons">icon2</div>
         <div className="icons">icon3</div>
       </div>
-      <div className="top">
+      <div className="top col-start-3 col-span-10 row-start-1 row-span-2 flex gap-4 p-4">
         <div className="icons">icon1</div>
         <div className="icons">icon2</div>
         <div className="icons">icon3</div>
         <div className="icons">icon4</div>
       </div>
-      <div className="center flex">
+      <div className="center col-start-3 col-span-8 row-start-3 row-span-8 flex items-center gap-4 p-4">
         <input
-          className="border-2 rounded-2xl"
+          className="border-2 rounded-4 px-2 py-1"
           type="text"
           value={Left}
           onChange={(e) => setLeft(e.target.value)}
@@ -27,7 +27,7 @@ export default function Dashboard() {
         />
         <div className="button">Center button</div>
         <input
-          className="border-2 rounded-2xl"
+          className="border-2 rounded-4 px-2 py-1"
           type="text"
           value={Right}
           onChange={(e) => setRight(e.target.value)}
@@ -35,7 +35,7 @@ export default function Dashboard() {
           required
         />
       </div>
-      <div className="right">
+      <div className="right col-start-11 col-span-2 row-start-3 row-span-10 flex flex-col gap-4 p-4">
         <div className="icons">icon1</div>
         <div className="icons">icon2</div>
         <div className="icons">icon3</div>
@@ -43,7 +43,7 @@ export default function Dashboard() {
         <div className="icons">icon5</div>
         <div className="icons">icon6</div>
       </div>
-      <div className="bottom">
+      <div className="bottom col-start-1 col-span-10 row-start-11 row-span-2 flex gap-4 p-4">
         <div className="icons">icon1</div>
         <div className="icons">icon2</div>
         <div className="icons">icon3</div>
